13cd2436235a058c3c962f5353a73769dcba9612
VRARStav <abzh.1805@gmail.com>
2024-04-04 17:24:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    """ Обработчик команды /start """
+    await message.answer(f'{message.from_user.first_name}, добро пожаловать!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    """ Обработчик неизвестного сообщения (любого, кроме известных) """
+    await message.reply('Я тебя не понимаю.')
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
